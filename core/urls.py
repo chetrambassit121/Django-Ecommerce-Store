24df2d0083b70b22a83143e3fa7a6afceb656f68
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-09 19:07:26 -0400

    created store url, created store views, created store templtes

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('store.urls'), name="stores"),
 ]
 
 if settings.DEBUG:
--- store/views.py
+++ store/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Category, Product
+
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'products': products})
