bff12df976bf7c914840002078d71db26e1b2026
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-09 18:43:29 -0400

    category and products basic testing

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -7,7 +7,7 @@ from store.models import Category, Product
 
 class TestCategoriesModel(TestCase):
 
-    def setUp(self):
+    def setUp(self):                                    # creating a category 
         self.data1 = Category.objects.create(name='django', slug='django')
 
     def test_category_model_entry(self):
@@ -15,6 +15,28 @@ class TestCategoriesModel(TestCase):
         Test Category model data insertion/types/field attributes
         """
         data = self.data1
-        self.assertTrue(isinstance(data, Category))
-        self.assertEqual(str(data), 'django')
+        self.assertTrue(isinstance(data, Category))    # testing the category feilds are true
+        self.assertEqual(str(data), 'django')          # testing the __str__ method for returning title 
+        
 
+    # def test_category_title_entry(self):
+    #     """
+    #     Test Category model data insertion/types/field attributes
+    #     """
+    #     data = self.data1
+    #     self.assertEqual(str(data), 'django')
+
+class TestProductsModel(TestCase):                     # creating products model 
+    def setUp(self):
+        Category.objects.create(name='django', slug='django')
+        User.objects.create(username='admin')
+        self.data1 = Product.objects.create(category_id=1, title='django beginners', created_by_id=1,
+                                            slug='django-beginners', price='20.00', image='django')
+
+    def test_products_model_entry(self):
+        """
+        Test product model data insertion/types/field attributes
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Product))      # testing product feilds are true
+        self.assertEqual(str(data), 'django beginners') # tsting the __str__ method for returning the string 
