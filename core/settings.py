c2a135d8768edda47a4e2c69b5251fb660fc6e6e
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-09 22:03:40 -0400

    created context processor file for our categories view

--- core/settings.py
+++ core/settings.py
@@ -64,7 +64,10 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'store.views.categories',
+
+                # 'store.views.categories',
+                'store.context_processors.categories',
+
             ],
         },
     },
