eb97cc58a8eaf08456b9582b8712cc3b36812911
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-11 11:55:57 -0400

    basket add logic

--- basket/basket.py
+++ basket/basket.py
@@ -13,5 +13,26 @@ class Basket():
         self.session = request.session
         basket = self.session.get('skey')
         if 'skey' not in request.session:
-            basket = self.session['skey'] = {'number':12345678}
+            basket = self.session['skey'] = {}
         self.basket = basket
+
+    def add(self, product, qty):
+        """
+        Adding and updating the users basket session data
+        """
+        product_id = str(product.id)
+
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+        else:
+            self.basket[product_id] = {'price': str(product.price), 'qty': qty}
+
+        self.session.modifed = True
+
+    def __len__(self):
+        """
+        Get the basket data and count the qty of items
+        """
+        return sum(item['qty'] for item in self.basket.values())
+
+
