7aa07a6618b3d4a20a41947320b1c4dc778af01c
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-11 18:49:14 -0400

    created custom account manager, userbase, updated core.settings, updated

--- core/settings.py
+++ core/settings.py
@@ -137,3 +137,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'account.UserBase'
+LOGIN_REDIRECT_URL = '/account/dashboard'
+LOGIN_URL = '/account/login/'
+
+# Email setting
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
