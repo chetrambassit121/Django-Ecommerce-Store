fb99af7ec123d5b05f2caf19167bb3738f62441f
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-11 15:03:04 -0400

    created update logic

--- basket/views.py
+++ basket/views.py
@@ -36,14 +36,14 @@ def basket_delete(request):
         return response
 
 
-# def basket_update(request):
-#     basket = Basket(request)
-#     if request.POST.get('action') == 'post':
-#         product_id = int(request.POST.get('productid'))
-#         product_qty = int(request.POST.get('productqty'))
-#         basket.update(product=product_id, qty=product_qty)
+def basket_update(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
+        basket.update(product=product_id, qty=product_qty)
 
-#         basketqty = basket.__len__()
-#         baskettotal = basket.get_total_price()
-#         response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
-#         return response
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
+        return response
