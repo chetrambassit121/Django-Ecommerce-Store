9f5dd3ff2d29ef077cdcff5436ecbce8b02024bb
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-12 12:54:11 -0400

    profile edit views, urls, templates

--- account/views.py
+++ account/views.py
@@ -1,19 +1,15 @@
-import django
-# from django.utils.encoding import force_str
-from django.utils.encoding import force_bytes, force_text
 from django.contrib.auth import login, logout
-# django.utils.encoding.force_text = force_str
 from django.contrib.auth.decorators import login_required
 from django.contrib.sites.shortcuts import get_current_site
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
-from django.utils.encoding import force_bytes
+from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
-# from orders.views import user_orders
 
-from .forms import RegistrationForm
+
+from .forms import RegistrationForm, UserEditForm
 from .models import UserBase
 from .tokens import account_activation_token
 
@@ -23,12 +19,36 @@ def dashboard(request):
     
     return render(request,
                   'account/user/dashboard.html',
-                  )
+                  {'section': 'profile'})
+
+
+@login_required
+def edit_details(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+
+        if user_form.is_valid():
+            user_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+
+    return render(request,
+                  'account/user/edit_details.html', {'user_form': user_form})
+
+
+@login_required
+def delete_user(request):
+    user = UserBase.objects.get(user_name=request.user)
+    user.is_active = False
+    user.save()
+    logout(request)
+    return redirect('account:delete_confirmation')
+
 
 def account_register(request):
 
-    # if request.user.is_authenticated:
-    #     return redirect('/')
+    if request.user.is_authenticated:
+        return redirect('account:dashboard')
 
     if request.method == 'POST':
         registerForm = RegistrationForm(request.POST)
