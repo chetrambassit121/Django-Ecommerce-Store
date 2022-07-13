4c4ab2d47dd02e3cd82db55c81610c142e9a12a2
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-13 13:08:49 -0400

    re developed store models, updated store views reflecting models

--- store/views.py
+++ store/views.py
@@ -4,16 +4,16 @@ from .models import Category, Product
 
 
 def product_all(request):
-    products = Product.products.all()
-    return render(request, 'store/index.html', {'products': products})
+    products = Product.objects.all()
+    return render(request, "store/index.html", {"products": products})
 
 
 def category_list(request, category_slug=None):
     category = get_object_or_404(Category, slug=category_slug)
     products = Product.products.filter(category=category)
-    return render(request, 'store/category.html', {'category': category, 'products': products})
+    return render(request, "store/category.html", {"category": category, "products": products})
 
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, 'store/single.html', {'product': product})
+    return render(request, "store/single.html", {"product": product})
