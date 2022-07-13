4fafa4f9be812167b3142e8d4d6a0e72c889e0d6
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-13 16:03:50 -0400

    implemented Address model, created urls views templates and CRUD operations based on the Adress model

--- account/forms.py
+++ account/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import (
     SetPasswordForm,
 )
 
-from .models import Customer
+from .models import Address, Customer
 
 
 class UserLoginForm(AuthenticationForm):
@@ -22,6 +22,31 @@ class UserLoginForm(AuthenticationForm):
             }
         )
     )
+
+
+class UserAddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = ["full_name", "phone", "address_line", "address_line2", "town_city", "postcode"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["full_name"].widget.attrs.update(
+            {"class": "form-control mb-2 account-form", "placeholder": "Full Name"}
+        )
+        self.fields["phone"].widget.attrs.update({"class": "form-control mb-2 account-form", "placeholder": "Phone"})
+        self.fields["address_line"].widget.attrs.update(
+            {"class": "form-control mb-2 account-form", "placeholder": "Full Name"}
+        )
+        self.fields["address_line2"].widget.attrs.update(
+            {"class": "form-control mb-2 account-form", "placeholder": "Full Name"}
+        )
+        self.fields["town_city"].widget.attrs.update(
+            {"class": "form-control mb-2 account-form", "placeholder": "Full Name"}
+        )
+        self.fields["postcode"].widget.attrs.update(
+            {"class": "form-control mb-2 account-form", "placeholder": "Full Name"}
+        )
 
 
 class RegistrationForm(forms.ModelForm):
