7750ab0ed7ad23974375add6747c6b5cd5b120b2
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-15 18:37:03 -0400

    registration errors in production

--- account/views.py
+++ account/views.py
@@ -98,66 +98,6 @@ def delete_user(request):
 #     return render(request, "account/registration/register.html", {"form": registerForm})
 
 
-# def account_register(request):
-#     if request.method == 'POST':
-#         registerForm = RegistrationForm(request.POST)
-#         if registerForm.is_valid():
-#             user = registerForm.save(commit=False)
-#             user.is_active = False
-#             user.save()
-#             current_site = get_current_site(request)
-#             mail_subject = 'Activate your blog account.'
-#             message = render_to_string(
-#                 "account/registration/account_activation_email.html",
-#                 {
-#                     'user': user,
-#                     'domain': current_site.domain,
-#                     'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-#                     'token': account_activation_token.make_token(user),
-#                 }
-#             )
-#             to_email = registerForm.cleaned_data.get('email')
-#             email = mail.EmailMessage(mail_subject, message, to=[to_email])
-#             email.send()
-#             return render(request, "account/registration/register_email_confirm.html")
-#     else:
-#         registerForm = RegistrationForm()
-#     return render(request, "account/registration/register.html", {"form": registerForm})
-
-
-
-# def account_register(request):
-
-#     if request.user.is_authenticated:
-#         return redirect("account:dashboard")
-
-#     if request.method == "POST":
-#         registerForm = RegistrationForm(request.POST)
-#         if registerForm.is_valid():
-#             user = registerForm.save(commit=False)
-#             user.email = registerForm.cleaned_data["email"]
-#             user.set_password(registerForm.cleaned_data["password"])
-#             user.is_active = False
-#             user.save()
-#             current_site = get_current_site(request)
-#             subject = "Activate your Account"
-#             message = render_to_string(
-#                 "account/registration/account_activation_email.html",
-#                 {
-#                     "user": user,
-#                     "domain": current_site.domain,
-#                     "uid": urlsafe_base64_encode(force_bytes(user.pk)),
-#                     "token": account_activation_token.make_token(user),
-#                 },
-#             )
-#             # user.email_user(subject=subject, message=message)
-#             to_email = registerForm.cleaned_data.get('email')
-#             email = mail.EmailMessage(subject, message, to=[to_email])
-#             email.send()
-#             return render(request, "account/registration/register_email_confirm.html", {"form": registerForm})
-#     else:
-#         registerForm = RegistrationForm()
-#     return render(request, "account/registration/register.html", {"form": registerForm})
 
 
 def account_register(request):
