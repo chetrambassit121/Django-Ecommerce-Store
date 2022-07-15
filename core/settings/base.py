303c6b6d15c9a6054c802e6d76676e09d312f554
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-15 14:05:20 -0400

    fixing deployment issues

--- core/settings/base.py
+++ core/settings/base.py
@@ -29,22 +29,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+# DEBUG = config('DEBUG')
+DEBUG = True
 
 # ALLOWED_HOSTS = config("ALLOWED_HOSTS")
 
 ALLOWED_HOSTS = ['*']
 
 
-# HTTPS settings
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-# SECURE_SSL_REDIRECT = True
 
-# HSTS settings
-SECURE_HSTS_SECONDS = 31536000 # 1 year
-SECURE_HSTS_PRELOAD = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+
 
 
 # Application definition
--- core/urls.py
+++ core/urls.py
@@ -6,13 +6,13 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("store.urls", namespace="store")),
-    path("basket/", include("basket.urls", namespace="basket")),
+    path("", include("store.urls"), name="store"),
+    path("basket/", include("basket.urls"), name="basket"),
     # path("payment/", include("payment.urls", namespace="payment")),
-    path("account/", include("account.urls", namespace="account")),
-    path("orders/", include("orders.urls", namespace="orders")),
+    path("account/", include("account.urls"), name="account"),
+    path("orders/", include("orders.urls"), name="orders"),
     path("__debug__/", include(debug_toolbar.urls)),
-    path("checkout/", include("checkout.urls", namespace="checkout")),
+    path("checkout/", include("checkout.urls"), name="checkout"),
 ]
 
 if settings.DEBUG:
