491fe636d527d942032060b618a9256f85ce652c
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-15 20:09:59 -0400

    registration errors in production

--- account/views.py
+++ account/views.py
@@ -137,7 +137,7 @@ def account_activate(request, uidb64, token):
     try:
         uid = force_str(urlsafe_base64_decode(uidb64))
         user = Customer.objects.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, user.DoesNotExist):
+    except(TypeError, ValueError, OverflowError, user.DoesNotExist):
         user = None
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
